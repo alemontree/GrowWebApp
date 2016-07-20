91f6aa38166aaee370e5f589237e6d8a2cbd7ac8
Mikhail <mishnade@gmail.com>
2016-07-20 10:48:05 -0700

    formController now works again

--- App/app.js
+++ App/app.js
@@ -28,56 +28,56 @@ function config($stateProvider, $urlRouterProvider) {
     {
       url: '/1',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view1.html")
     })
     .state('view2',
     {
       url: '/2',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view2.html")
     })
     .state('view3',
     {
       url: '/3',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view3.html")
     })
     .state('view4',
     {
       url: '/4',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view4.html")
     })
     .state('view5',
     {
       url: '/5',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view5.html")
     })
     .state('view6',
     {
       url: '/6',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view6.html")
     })
     .state('view7',
     {
       url: '/7',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view7.html")
     })
     .state('view8',
     {
       url: '/8',
       transclude: true,
-      controller: 'FormController',
+      controller: 'formController',
       template: require("./Partials/view8.html")
     })
 }
