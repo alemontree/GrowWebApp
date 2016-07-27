d03783bf266642cbccfbf79dbb338fc24bcba5d0
Mikhail <mishnade@gmail.com>
2016-07-27 12:52:44 -0700

    fix html

--- App/Components/Form/form-directive.js
+++ App/Components/Form/form-directive.js
@@ -3,22 +3,23 @@ import angular from 'angular';
 
 
   // this directive checks if the second pw is equal to the first
-function compareTo() {
-    return {
-        require: "ngModel",
-        scope: {
-            otherModelValue: "=compareTo"
-        },
-        link: function(scope, element, attributes, ngModel) {
-             
-            ngModel.$validators.compareTo = function(modelValue) {
-                return modelValue == scope.otherModelValue;
-            }; 
-            scope.$watch("otherModelValue", function() {
-                ngModel.$validate();
-            });
-        }
-    };
+let compareTo = function()  {
+  return {
+    require: "ngModel",
+    controllerAs: 'formCtrl',
+    scope: {
+      otherModelValue: "=compareTo"
+    },
+    link: function(scope, element, attributes, ngModel) {
+       
+      ngModel.$validators.compareTo = function(modelValue) {
+        return modelValue == this.otherModelValue;
+      }; 
+      scope.$watch("otherModelValue", function() {
+        ngModel.$validate();
+      });
+    }
+  };
 };
 
 export default compareTo;
